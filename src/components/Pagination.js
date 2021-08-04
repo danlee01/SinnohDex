f2fd565e84a950e0ff34e5ec93d78ffcd4f07111
Dan Lee <danlee01@ucla.edu>
2021-08-03 20:01:52 -0700

    complete overhaul

---
+++ src/components/Pagination.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+    const pageNumbers = []
+
+    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+ 
+    const createPage = (number) => {
+        if (number === currentPage) {
+            return ((
+                <li key={number} className="page-item active">
+                    <a onClick={() => paginate(number)} href="!#" className="page-link">
+                        {number}
+                    </a>
+                </li>
+            ))
+        } else {
+            return (
+                <li key={number} className="page-item ">
+                    <a onClick={() => paginate(number)} href="!#" className="page-link">
+                        {number}
+                    </a>
+                </li>
+            )
+        }
+    }
+
+    return (
+        <nav>
+            <ul className="pagination justify-content-center" >
+                {pageNumbers.map(number => (
+                        createPage(number)
+                    ))}
+            </ul>
+        </nav>
+    );
+}
+
+export default Pagination;
---
+++ src/components/Posts.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+
+import Pokedex from 'pokedex-promise-v2';
+const p = new Pokedex();
+
+const Posts = ({ posts, loading, indexOfFirst }) => {
+    const getName = (name) => {
+        /* HOTFIX: PokeAPI names don't always match with Pokemon Showdown assets */
+        if (name === "mr-mime") {
+            return "mrmime";
+        } else if (name === "mime-jr") {
+            return "mimejr";
+        } else if (name === "wormadam-plant"){
+            return "wormadam";
+        } else {
+            return name;
+        }
+    };
+
+    const formatEntry = (number) => {
+        var len = Math.log(number) * Math.LOG10E + 1 | 0;
+        console.log(`${len}`)
+        if (len === 1) {
+            return '00' + number;
+        } else if (len === 2) {
+            return '0' + number;
+        } else {
+            return number;
+        }
+    };
+
+    const getTypes = (types) => {
+        const resArray = types.map( type => type.type.name );
+        return resArray.join(', ');
+    };
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    console.log(posts)
+    return (
+        <div className="posts-container">
+            <ul id="pokedex">
+                {posts.map((post, i) => (
+                    <li key={i} className="pokemon">
+                        <img className="pokemon-image" src={`https://play.pokemonshowdown.com/sprites/xyani/${getName(post.name)}.gif`}/>
+                        <h2 className="pokemon-title">{getName(post.name)}</h2>
+                        <p className="pokemon-subtitle">{getTypes(post.types)}</p>
+                        <h3 className="pokemon-number">{formatEntry(indexOfFirst+1+i)}</h3>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Posts
